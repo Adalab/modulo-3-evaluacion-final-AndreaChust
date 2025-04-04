51d6472f69cb71f9df76b216bb3baea91f744ad9
AndreaChust <andrea.chust@hotmail.com>
2025-04-04 09:40:20 +0200

    components

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,19 @@
 import "../scss/App.scss";
+import CharacterList from "./CharacterList";
+import Filters from "./Filters";
 
 function App() {
-
   return (
     <>
+      <header>
+        <h3>Ricky Morty</h3>
+      </header>
+      <Filters />
+      <main>
+        <CharacterList />
+      </main>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/CharacterList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterCard from './CharacterCard';
+
+function CharacterList() {
+  return (
+      <section>
+        <ul><CharacterCard /></ul>
+      </section>
+  )
+}
+
+export default CharacterList
